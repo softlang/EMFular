0f488ee6310e0720031e7b02f44df06fc437b4ad
Susanne Göbel <goebel@uni-koblenz.de>
2026-02-05 08:49:24 +0100

    Fix karma conf, also adapt tests to work with new reduced effort for serializtion and deserialization

--- projects/karma.conf.ts
+++ projects/karma.conf.ts
@@ -1,31 +1,43 @@
-// Karma configuration file, see link for more information
+// Karma configuration file
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-module.exports = function (config) {
+
+
+module.exports = function (config: any) {
     config.set({
         basePath: '',
         frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
         plugins: [
             require('karma-jasmine'),
-            require('karma-firefox-launcher'),
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
-            require('karma-coverage-istanbul-reporter'),
+            require('karma-coverage'),
             require('@angular-devkit/build-angular/plugins/karma')
         ],
+
         client: {
-            clearContext: false // leave Jasmine Spec Runner output visible in browser
+            clearContext: false // keep Jasmine Spec Runner output visible
         },
-        coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, '../coverage/[app-name]'),
-            reports: ['html', 'lcovonly', 'text-summary'],
-            fixWebpackSourcePaths: true
+
+        coverageReporter: {
+            dir: require('path').join(__dirname, '../coverage'),
+            reporters: [
+                { type: 'html' },
+                { type: 'lcovonly' },
+                { type: 'text-summary' }
+            ]
         },
+
         reporters: ['progress', 'kjhtml'],
+
         port: 8080,
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome'],
+
+        // Recommended for Angular CLI
+        browsers: ['ChromeHeadless'],
+
         singleRun: true,
         restartOnFileChange: true
     });
