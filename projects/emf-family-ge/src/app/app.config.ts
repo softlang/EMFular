8e8b182c860963070f6279ab1b23f0a59b9307a8
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-05 21:35:46 +0100

    INitialize Historyservice with right timing (when window is available)

--- projects/emf-family-ge/src/app/app.config.ts
+++ projects/emf-family-ge/src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, inject, PLATFORM_ID, provideAppInitializer, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {isPlatformBrowser} from '@angular/common';
+import {FamilyHistoryService} from './shared/basicfamily/edit/family-history.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAppInitializer(()=>{
+      const platformId = inject(PLATFORM_ID);
+      if(isPlatformBrowser(platformId)){
+        const history: FamilyHistoryService = inject(FamilyHistoryService);
+        history.init();
+      }
+    })
+  ]
 };
