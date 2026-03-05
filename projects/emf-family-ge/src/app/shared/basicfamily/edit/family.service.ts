c00e305dbf79660ec83e140461407c7ec6d9166e
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-05 11:07:11 +0100

    Add explicit usage of man and woman inside service to avoid angular's aggressive tree shaking

--- projects/emf-family-ge/src/app/shared/basicfamily/edit/family.service.ts
+++ projects/emf-family-ge/src/app/shared/basicfamily/edit/family.service.ts
@@ -3,11 +3,18 @@ import {ModelService} from 'ngx-emfular-integration';
 import {IoService} from 'ngx-emfular-helper';
 import { Family } from '../core/family';
 import { FamilyHistoryService } from './family-history.service';
+import { Woman, Man } from '../core/persons';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FamilyService extends ModelService<Family>{
+
+  //explicitly use modeling classes to avoid treesgaking them away:
+  womanAgainstExtinction: Woman = new Woman();
+  manAgainstExtinction: Man = new Man();
+
 
   constructor(
     historyService: FamilyHistoryService,
