63adbf71de2dcbc335bd192ec7498989df7c2947
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-06 12:15:25 +0100

    Propagate clicls on real model elements to main tree editor, it can choose whom to call

--- projects/emf-family-ge/src/app/app.component.ts
+++ projects/emf-family-ge/src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {TreeEditorComponent } from "ngx-emfular-integration"
+import {TreeEditorComponent, BasicModelDetailsService } from "ngx-emfular-integration"
 import { FamilyService } from './shared/basicfamily/edit/family.service';
+import {Family} from './shared/basicfamily/core/family';
+import {Referencable} from 'emfular';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,12 @@ import { FamilyService } from './shared/basicfamily/edit/family.service';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'emf-family-ge';
-  constructor(public modelService: FamilyService){}
+  title = 'Basic Family';
+  constructor(
+    protected modelService: FamilyService,
+    protected detailsService: BasicModelDetailsService){}
+
+  chooseElement(referencable: Referencable<any>) {
+    this.detailsService.openDetails(referencable, this.modelService);
+  }
 }
