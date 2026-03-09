038e837f000ffea105f26f6023ae060375115db5
Susanne Göbel <goebel@uni-koblenz.de>
2026-03-09 09:42:17 +0100

    Offering to overwrite the basic details service on tree components, but using it as default, hence the app component needs to do even less

--- projects/emf-family-ge/src/app/app.component.ts
+++ projects/emf-family-ge/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {TreeEditorComponent, BasicModelDetailsService } from "ngx-emfular-integration"
+import { TreeEditorComponent } from "ngx-emfular-integration"
 import { FamilyService } from './shared/basicfamily/edit/family.service';
-import {Referencable} from 'emfular';
-import {Family} from './shared/basicfamily/core/family';
+
 
 @Component({
   selector: 'app-root',
@@ -15,10 +14,6 @@ export class AppComponent {
   title = 'Basic Family';
   constructor(
     protected modelService: FamilyService,
-    protected detailsService: BasicModelDetailsService<Family>
   ){}
 
-  chooseElement(referencable: Referencable<any>) {
-    this.detailsService.openDetails(referencable, this.modelService);
-  }
 }
